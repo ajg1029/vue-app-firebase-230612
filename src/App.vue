2735472a9b61e445b59f66e322d207e8b77e3d22
ajg1029 <urban.seonbi.deon@gmail.com>
2023-06-13 11:49:17 +0900

    firestore 실시간

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <TestFirebase msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import TestFirebase from './components/TestFirebase.vue';
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
+    TestFirebase
   }
 }
 </script>
---
+++ src/components/TestFirebase.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestFirebase',
+  props: {
+    msg: String
+  },
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+}
+
+</script>
+
+<style>
+
+</style>
